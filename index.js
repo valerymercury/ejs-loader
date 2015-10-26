64b2c18e60af283d1be4750a49f66a6de220c3ad
Valery Bugakov <bugakov@mercurydevelopment.com>
2015-10-26 15:02:05 +0400

    add removeNewlines option

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var loaderUtils = require("loader-utils");
 module.exports = function (source) {
   this.cacheable && this.cacheable();
   var options = loaderUtils.parseQuery(this.query);
+  if (options.removeNewlines == true) {
+    source = source.replace(/<!--.*-->/g, '').replace(/\s{2,}/gm, ' ').replace(/\n/g, '');
+  }
   var template = _.template(source, options);
   return 'module.exports = ' + template;
 };
